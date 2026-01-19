d25c119c64c332d4c27b46179686ef32bd8fab27
Aldiyar <mc.aaldy@gmail.com>
2026-01-20 00:48:01 +0500

    minor fix of app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -342,7 +342,7 @@ export default function App() {
         {renderPage()}
       </main>
       
-      {!isAdminPage && <Footer onNavigate={handleNavigate} />}
+      {<Footer onNavigate={handleNavigate} />}
     </div>
   );
 }
